b796cd1b7b4ac3c2d2328aeb9cdbdeb810f36e36
Craig Gunson <craig.gunson@rea-group.com>
2019-07-29 10:42:55 +1000

    tidy

--- mountains.js
+++ mountains.js
@@ -1,5 +1,3 @@
-
-
 var choose = 0;
 var maxy = 1000;
 var canvas;
@@ -10,139 +8,120 @@ var y;
 var size;
 var x;
 
-function tree(x,y,size) {
-  ctx.translate(0,+size/4);
-  ctx.lineTo(x,y)
-  ctx.lineTo(x-(size/2),y);
-  ctx.lineTo(x,y-size/2);
-  ctx.lineTo(x+(size/2),y);
-  ctx.lineTo(x,y);
-  ctx.translate(0,-size/4)
-  ctx.lineTo(x,y)
-  ctx.lineTo(x-(size/2.5),y);
-  ctx.lineTo(x,y-size/2.5);
-  ctx.lineTo(x+(size/2.5),y);
-  ctx.lineTo(x,y);
-  ctx.translate(0,-size/4)
-  ctx.lineTo(x,y)
-  ctx.lineTo(x-(size/3),y);
-  ctx.lineTo(x,y-size/3);
-  ctx.lineTo(x+(size/3),y);
-  ctx.lineTo(x,y);
-  ctx.translate(0,(size/4+size/4-size/4));
-  ctx.lineTo(x,y);
-  //ctx.lineTo(100,100);
-  
-  ctx.fill();
-}
 
 function coinFlip() {
-    return Math.floor(Math.random() * 2);
+  return Math.floor(Math.random() * 2);
 }
 
 
 function draw() {
-    ctx.beginPath();
-    ctx.moveTo(0, wh);
+  ctx.beginPath();
+  ctx.moveTo(0, wh);
 
-    for (var x = 0; x < ww+50; x=x+30+(Math.random()*10)) {
-      choose = coinFlip();
-      altitude = Math.floor(Math.random()*25)
-      if (choose == 0) {y = y - altitude;}
-      else {y = y + altitude;
+  for (var x = 0; x < ww + 50; x = x + 30 + (Math.random() * 10)) {
+    choose = coinFlip();
+    altitude = Math.floor(Math.random() * 25)
+    if (choose == 0) {
+      y = y - altitude;
+    } else {
+      y = y + altitude;
 
-            }
-      if (y < maxy) {maxy = y;}
-      ctx.lineTo(x,y);
-      choose = coinFlip();
-       // if (choose == 0) {tree(x,y,size);}
-      //console.log('x',x,'y',y,choose,maxy);
     }
-    ctx.lineTo(ww,wh);
-    ctx.fill();
+    if (y < maxy) {
+      maxy = y;
+    }
+    ctx.lineTo(x, y);
+    choose = coinFlip();
+
+  }
+  ctx.lineTo(ww, wh);
+  ctx.fill();
 
 }
 
- window.onorientationchange = function() {
-        var orientation = window.orientation;
-            switch(orientation) {
-                case 0:
-                case 90:
-                case -90: window.location.reload();
-                break; }
-    };
+window.onorientationchange = function () {
+  var orientation = window.orientation;
+  switch (orientation) {
+    case 0:
+    case 90:
+    case -90:
+      window.location.reload();
+      break;
+  }
+};
 
 
 function init() {
   window.addEventListener('resize', init, false);
-  window.addEventListener("orientationchange", function() {
-  window.location.reload();});
+  window.addEventListener("orientationchange", function () {
+    window.location.reload();
+  });
 
-canvas = document.getElementById('canvas');
-ctx = canvas.getContext('2d');
-wh = window.innerHeight;
-ww = window.innerWidth;
-ctx.canvas.width  = ww;
-ctx.canvas.height = wh;
-console.log('windowsize',ww,wh);
-var my_gradient = ctx.createLinearGradient(0, 0, 0, wh/2);
-my_gradient.addColorStop(0, "rgba(200, 100, 50, 1)");
-my_gradient.addColorStop(1, "rgba(250, 200, 100, 1)");
-ctx.fillStyle = my_gradient;
-ctx.fillRect(0, 0, ww, wh);
-render();
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+  wh = window.innerHeight;
+  ww = window.innerWidth;
+  ctx.canvas.width = ww;
+  ctx.canvas.height = wh;
+  //console.log('windowsize', ww, wh);
+  var my_gradient = ctx.createLinearGradient(0, 0, 0, wh / 2);
+  my_gradient.addColorStop(0, "rgba(200, 100, 50, 1)");
+  my_gradient.addColorStop(1, "rgba(250, 200, 100, 1)");
+  ctx.fillStyle = my_gradient;
+  ctx.fillRect(0, 0, ww, wh);
+  render();
 }
 
 init();
 
 function render() {
 
-y = wh/3;
-size = 20
-ctx.fillStyle = "rgba(200, 50, 50, 1)";
-draw();
+  y = wh / 3;
+  size = 20
+  ctx.fillStyle = "rgba(200, 50, 50, 1)";
+  draw();
 
-size = 35
-y = wh/2;
-ctx.fillStyle = "rgba(140, 20, 40, 1)";
-draw();
+  size = 35
+  y = wh / 2;
+  ctx.fillStyle = "rgba(140, 20, 40, 1)";
+  draw();
 
-size = 50
-y = wh-(wh/3);
-ctx.fillStyle = "rgba(80, 10, 40, 1)";
-draw();
+  size = 50
+  y = wh - (wh / 3);
+  ctx.fillStyle = "rgba(80, 10, 40, 1)";
+  draw();
 
-function moon() {
-var moonplace = ww*Math.random()
-var moonheight = maxy-wh/40
+  function sun() {
+    var sunplace = ww * Math.random()
+    var sunheight = maxy - wh / 40
 
-for (var ang = 0; ang <= 1.9; ang=ang+.2) {
-  ctx.beginPath();
-  ctx.lineTo(moonplace, moonheight);
-  ctx.arc(moonplace, moonheight, 9000, ang * Math.PI, (ang+.1) * Math.PI);
-  ctx.lineTo(moonplace, moonheight);
-  ctx.fillStyle = "rgba(255,255,255,.05)";
-  ctx.fill();
-  ctx.closePath();
+    for (var ang = 0; ang <= 1.9; ang = ang + .2) {
+      ctx.beginPath();
+      ctx.lineTo(sunplace, sunheight);
+      ctx.arc(sunplace, sunheight, 9000, ang * Math.PI, (ang + .1) * Math.PI);
+      ctx.lineTo(sunplace, sunheight);
+      ctx.fillStyle = "rgba(255,255,255,.05)";
+      ctx.fill();
+      ctx.closePath();
 
-  }
+    }
 
-  for (var ang = .1; ang <= 2; ang=ang+.2) {
+    for (var ang = .1; ang <= 2; ang = ang + .2) {
+      ctx.beginPath();
+      ctx.lineTo(sunplace, sunheight);
+      ctx.arc(sunplace, sunheight, wh / 20, ang * Math.PI, (ang + .1) * Math.PI);
+      ctx.lineTo(sunplace, sunheight);
+      ctx.fillStyle = "rgba(255,255,255,.05)";
+      ctx.fill();
+      ctx.closePath();
+    }
+
     ctx.beginPath();
-    ctx.lineTo(moonplace, moonheight);
-    ctx.arc(moonplace, moonheight, wh/20, ang * Math.PI, (ang+.1) * Math.PI);
-    ctx.lineTo(moonplace, moonheight);
-    ctx.fillStyle = "rgba(255,255,255,.05)";
+    ctx.arc(sunplace, sunheight, wh / 30, 0, 2 * Math.PI);
+    ctx.fillStyle = "rgba(255,255,255,.1)";
     ctx.fill();
     ctx.closePath();
   }
 
-  ctx.beginPath();
-  ctx.arc(moonplace, moonheight, wh/30, 0, 2 * Math.PI);
-  ctx.fillStyle = "rgba(255,255,255,.1)";
-  ctx.fill();
-  ctx.closePath();
-}
-
-moon();
+  sun();
 }
