56ea9b068c6c654e548cdc43a05b1830c32ed226
craiggunson <cbgunson@gmail.com>
2023-03-31 07:25:29 +1100

    fix: removed class errors

--- refactor/src/App.js
+++ refactor/src/App.js
@@ -4,47 +4,46 @@ function App() {
   return (
     <div className="App">
 
-      <body>
   <main>
-    <div class="section no-pad-bot" id="index-banner">
-      <div class="container">
+    <div className="section no-pad-bot" id="index-banner">
+      <div className="container">
         
-        <h1 class="header center">Craig Gunson</h1>
-        <div class="row center">
-          <h3 class="header col s12 light">Doing what I can, with what I have, where I am.</h3>
+        <h1 className="header center">Craig Gunson</h1>
+        <div className="row center">
+          <h3 className="header col s12 light">Doing what I can, with what I have, where I am.</h3>
         </div>
         
       </div>
     </div>
-    <div class="container">
-      <div class="section">
-        <div class="row">
-          <div class="col s12 m4">
-            <div class="icon-block">
-              <h2 class="center white-text"><i class="fas fa-cloud fa-2x"></i></h2>
-              <h5 class="center">Cloud Tradie</h5>
+    <div className="container">
+      <div className="section">
+        <div className="row">
+          <div className="col s12 m4">
+            <div className="icon-block">
+              <h2 className="center white-text"><i className="fas fa-cloud fa-2x"></i></h2>
+              <h5 className="center">Cloud Tradie</h5>
 
-              <p class="center light white-text">Delivering value with cloud technology to enable customer
+              <p className="center light white-text">Delivering value with cloud technology to enable customer
                 success. DevOps, Coaching, design, build on AWS or GCP.</p>
             </div>
           </div>
 
-          <div class="col s12 m4">
-            <div class="icon-block">
-              <h2 class="center white-text"><i class="fas fa-wrench fa-2x"></i></h2>
-              <h5 class="center">Engineer</h5>
+          <div className="col s12 m4">
+            <div className="icon-block">
+              <h2 className="center white-text"><i className="fas fa-wrench fa-2x"></i></h2>
+              <h5 className="center">Engineer</h5>
 
-              <p class="center light white-text">Curious how things work, comfortable hands on. Keeping it simple &
+              <p className="center light white-text">Curious how things work, comfortable hands on. Keeping it simple &
                 sustainable. </p>
             </div>
           </div>
 
-          <div class="col s12 m4">
-            <div class="icon-block">
-              <h2 class="center white-text"><i class="fas fa-code fa-2x"></i></h2>
-              <h5 class="center">Innovator</h5>
+          <div className="col s12 m4">
+            <div className="icon-block">
+              <h2 className="center white-text"><i className="fas fa-code fa-2x"></i></h2>
+              <h5 className="center">Innovator</h5>
 
-              <p class="center light white-text">Infrastructure or code, frontend or backend.  Like to muck with UI/UX & data visualization.</p>
+              <p className="center light white-text">Infrastructure or code, frontend or backend.  Like to muck with UI/UX & data visualization.</p>
             </div>
           </div>
         </div>
@@ -53,18 +52,18 @@ function App() {
       </div>
   </main>
 
-  <footer class="page-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col l6 s12">
+  <footer className="page-footer">
+    <div className="container">
+      <div className="row">
+        <div className="col l6 s12">
           <h5>Bio</h5>
-          <p class="white-text">Raised in New Zealand, now home is the Mornington Peninsula.  I am passionate
+          <p className="white-text">Raised in New Zealand, now home is the Mornington Peninsula.  I am passionate
             about technology and making it work for people. I spend my weekends outdoors, and weekdays on the computer. 
               If you have a technology problem you need solved, say <a href = "mailto: hello@craiggunson.com"><u>hello@craiggunson.com</u></a> </p>
 
         </div>
 
-        <div class="col l3 s12">
+        <div className="col l3 s12">
           <h5><br/> Projects</h5>
           <a href="https://kitesurf.craiggunson.com"><img style={{height: '1em'}} src='pineapple.png' alt="pineapple"/> Serverless Kite Surfing</a><br/>
           <a href="https://tidebubble.craiggunson.com"><img style={{height:'1em'}} src='droplet.png' alt="droplet"/> Serverless High Tide</a><br/>
@@ -74,18 +73,18 @@ function App() {
           <a href="http://cart.craiggunson.com"><img style={{height:'1em'}} src='box.png' alt="box"/>Fruit Cart GA4</a><br/>
         </div>
 
-        <div class="col l3 s12">
+        <div className="col l3 s12">
           <h5>Socials</h5>
-          <a href="https://github.com/craiggunson"><i class="fab fa-github fa-3x" title="Github"></i></a>
-          <a href="https://www.linkedin.com/in/craiggunson/"><i class="fab fa-linkedin-in fa-3x" title="Linkedin"></i></a>
-          <a href="https://www.imdb.com/name/nm6043920/"><i class="fab fa-imdb fa-3x" title="IMDB"></i></a>
+          <a href="https://github.com/craiggunson"><i className="fab fa-github fa-3x" title="Github"></i></a>
+          <a href="https://www.linkedin.com/in/craiggunson/"><i className="fab fa-linkedin-in fa-3x" title="Linkedin"></i></a>
+          <a href="https://www.imdb.com/name/nm6043920/"><i className="fab fa-imdb fa-3x" title="IMDB"></i></a>
 
         </div>
       </div>
     </div>
 
-    <div class="footer-copyright">
-      <div class="container">
+    <div className="footer-copyright">
+      <div className="container">
         Aww Yeah!!!
       </div>
     </div>
@@ -93,11 +92,11 @@ function App() {
   </footer>
 
 
-
+  <canvas id="canvas" className="background"></canvas>
  
 
 
-</body>
+
 </div>
   );
 }
