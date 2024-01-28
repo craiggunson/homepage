c2558a64d9ade477ba31f57ffe78a6b27bfe18ea
Craig Gunson <craiggunson@users.noreply.github.com>
2024-01-28 17:35:39 +1100

    Tidy - clean up some fonts and accessibility issues. (#21)
    * fix: tidy fonts
    
    * fix: brighten font
    
    * fix: heading order
    
    * fix: headings again

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         
         <h1 className="header center">Craig Gunson</h1>
         <div className="row center">
-          <h3 className="header col s12 light">Doing what I can, with what I have, where I am.</h3>
+          <h2 className="header col s12 light">Doing what I can, with what I have, where I am.</h2>
         </div>
         
       </div>
@@ -24,8 +24,8 @@ function App() {
         <div className="row">
           <div className="col s12 m4">
             <div className="icon-block">
-              <h2 className="center white-text"><i className="fas fa-cloud fa-2x"></i></h2>
-              <h5 className="center">Cloud Tradie</h5>
+              <h2 className="center white-text"><i className="fa-solid fa-cloud-bolt fa-2x"></i></h2>
+              <h3 className="center">Cloud Tradie</h3>
 
               <p className="center light white-text">Delivering value with cloud technology to enable customer
                 success. DevOps, Coaching, design, build on AWS or GCP.</p>
@@ -34,8 +34,8 @@ function App() {
 
           <div className="col s12 m4">
             <div className="icon-block">
-              <h2 className="center white-text"><i className="fas fa-wrench fa-2x"></i></h2>
-              <h5 className="center">Engineer</h5>
+              <h2 className="center white-text"><i className="fa-solid fa-tree fa-2x"></i></h2>
+              <h3 className="center">Engineer</h3>
 
               <p className="center light white-text">Curious how things work, comfortable hands on. Keeping it simple &
                 sustainable. </p>
@@ -44,8 +44,8 @@ function App() {
 
           <div className="col s12 m4">
             <div className="icon-block">
-              <h2 className="center white-text"><i className="fas fa-code fa-2x"></i></h2>
-              <h5 className="center">Innovator</h5>
+              <h2 className="center white-text"><i className="fa-solid fa-seedling fa-2x"></i></h2>
+              <h3 className="center">Innovator</h3>
 
               <p className="center light white-text">Infrastructure or code, frontend or backend.  Like to muck with UI/UX & data visualization.</p>
             </div>
@@ -60,7 +60,7 @@ function App() {
     <div className="container">
       <div className="row">
         <div className="col l6 s12">
-          <h5>Bio</h5>
+          <h4>Bio</h4>
           <p className="white-text">Raised in New Zealand, now home is the Mornington Peninsula.  I am passionate
             about technology and making it work for people. I spend my weekends outdoors, and weekdays on the computer. 
               If you have a technology problem you need solved, say <a href = "mailto: hello@craiggunson.com"><u>hello@craiggunson.com</u></a> </p>
@@ -68,20 +68,21 @@ function App() {
         </div>
 
         <div className="col l3 s12">
-          <h5><br/> Projects</h5>
-          <a href="https://kitesurf.craiggunson.com"><img style={{height: '1em'}} src='pineapple.png' alt="pineapple"/> Serverless Kite Surfing</a><br/>
-          <a href="https://tidebubble.craiggunson.com"><img style={{height:'1em'}} src='droplet.png' alt="droplet"/> Serverless High Tide</a><br/>
-          <a href="https://github.com/craiggunson/weather-pi"><img style={{height: '1em'}} src='pi.png' alt="pi"/> IoT Pi Weather</a><br/>
-          <a href="https://github.com/craiggunson/mizaru"><img style={{height: '1em'}} src='pear.png' alt="pear"/> AI Emotion detection</a><br/>
-          <a href="https://github.com/craiggunson/sweet-n-sour"><img style={{height:'1em'}} src='sour.png' alt="sour"/> GraphQL Sweet-n-Sour</a><br/>
-          <a href="https://shopcart.craiggunson.com"><img style={{height:'1em'}} src='box.png' alt="box"/>Fruit Cart GA4</a><br/>
+          <h4><br/> Projects</h4>
+          <a href="https://kitesurf.craiggunson.com"> <projecticon>🍍</projecticon>  Serverless Kite Surfing</a><br/>
+          <a href="https://tidebubble.craiggunson.com"><projecticon>💧</projecticon> Serverless High Tide</a><br/>
+          <a href="https://github.com/craiggunson/weather-pi"><projecticon>🥧</projecticon> IoT Pi Weather</a><br/>
+          <a href="https://github.com/craiggunson/mizaru"><projecticon>🙈</projecticon> AI Emotion detection</a><br/>
+          <a href="https://github.com/craiggunson/sweet-n-sour"><projecticon>😝</projecticon> GraphQL Sweet-n-Sour</a><br/>
+          <a href="https://shopcart.craiggunson.com"><projecticon>📦</projecticon>Fruit Cart GA4</a><br/>
         </div>
 
         <div className="col l3 s12">
-          <h5>Socials</h5>
+          <h4>Socials</h4>
           <a href="https://github.com/craiggunson"><i className="fab fa-github fa-3x" title="Github"></i></a>
           <a href="https://www.linkedin.com/in/craiggunson/"><i className="fab fa-linkedin-in fa-3x" title="Linkedin"></i></a>
           <a href="https://www.imdb.com/name/nm6043920/"><i className="fab fa-imdb fa-3x" title="IMDB"></i></a>
+          <a href="https://www.instagram.com/beachcroozer/"><i className="fab fa-instagram fa-3x" title="Instagram"></i></a>
 
         </div>
       </div>
