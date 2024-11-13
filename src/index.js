a2562749fa2490a41f16a7c30a0e973a62bf2e63
Craig Gunson <craiggunson@users.noreply.github.com>
2024-11-13 03:42:15 +0000

    combine css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import Canvas from './Canvas';
 import reportWebVitals from './reportWebVitals';
