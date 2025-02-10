02d54c2e83ef72da7ac0c2f53e33750a226a1f8b
Craig Gunson <cbgunson@gmail.com>
2025-02-11 10:00:33 +1100

    fix: new motivational quote

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
         
         <h1 className="header center">Craig Gunson</h1>
         <div className="row center">
-          <h2 className="header col s12 light">Doing what I can, with what I have, where I am.</h2>
+          <h2 className="header col s12 light">Doing what I can, with what I have, where I am.<br/>
+          What stands in the way becomes the way</h2>
         </div>
         
       </div>
